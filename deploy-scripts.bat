af38fafb893654867936b67ef87ce785a607dbc0
PMichalik <paul.michalik@harman.com>
2016-07-08 18:09:15 +0200

    Location for deployment.

--- deploy-scripts.bat
+++ deploy-scripts.bat
@@ -6,6 +6,6 @@ set "SourceDir=%~dp0"
 if %SourceDir:~-1%==\ set "SourceDir=%SourceDir:~0,-1%"
 set "DestDir=\\hbi.ad.harman.com\munich\Public\_User\PMichalik\devenv.devkit"
 
-robocopy "%SourceDir%" "%DestDir%" *.bat Readme.txt
+robocopy "%SourceDir%" "%DestDir%" *.bat Readme.txt packages.txt
 
 endlocal
