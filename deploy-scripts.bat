059958e8767c8bf337a69024cf2304148b1b6a9c
PMichalik <paul.michalik@harman.com>
2016-07-08 18:03:36 +0200

    Added deployment script.

--- deploy-scripts.bat
+++ deploy-scripts.bat
@@ -2,4 +2,10 @@
 
 setlocal
 
+set "SourceDir=%~dp0"
+if %SourceDir:~-1%==\ set "SourceDir=%SourceDir:~0,-1%"
+set "DestDir=\\hbi.ad.harman.com\munich\Public\_User\PMichalik\devenv.devkit"
+
+robocopy "%SourceDir%" "%DestDir%" *.bat Readme.txt
+
 endlocal
