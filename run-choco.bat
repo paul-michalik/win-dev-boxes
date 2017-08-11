0ad9978867104e1727ce96b1ef11bdd41e3d0cd4
PMichalik <paul.michalik@harman.com>
2017-08-11 19:44:56 +0200

    Added packages-win7-devbox.config Change command syntax

--- run-choco.bat
+++ run-choco.bat
@@ -2,21 +2,17 @@
 
 rem ==============
 rem Usage: 
-rem run-choco.bat <choco-operation> <package-file-name>
+rem run-choco.bat <choco-operation> <packages-or-package-config>
 rem ==============
 
 setlocal
-set "PackageList=packages-win7-devbox-host.txt"
+set "PackageList="
 set "ChocoOperation=%~1"
 if /i not "%~2"=="" set "PackageList=%~2"
-set "PackageList=%~dp0%PackageList%"
 
 rem choco feature enable -n stopOnFirstPackageFailure 
 choco upgrade chocolatey /y /d /v
 choco install dotnet4.7 /y /d /v
-
-for /f "tokens=*" %%f in (%PackageList%) do (
-    choco %ChocoOperation% /y /d /v %%f
-)
+choco %ChocoOperation% /y /d /v %PackageList%
 
 endlocal
