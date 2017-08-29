b8329c7b13ff274ed34fc575464d0919c9c8a53c
PMichalik <paul.michalik@harman.com>
2017-08-29 18:57:57 +0200

    Switched to combination of plain scripts and a config file

--- install.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-setlocal
-
-start "install" cmd /k call "%~dp0run-choco.bat" install %*
-endlocal
--- uninstall.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-setlocal
-
-start "uninstall" cmd /k call "%~dp0run-choco.bat" uninstall %*
-
-endlocal
