b167a2c8fdebda55e43d52468e5e282bfc0ca894
PMichalik <paul.michalik@harman.com>
2017-08-31 18:11:39 +0200

    No shell restarts required.

--- win7-devbox.bat
+++ win7-devbox.bat
@@ -1,10 +1,10 @@
 @echo off
 rem ====================
-rem Run in new shells in order to ensure properly updated environments.
+rem Bootstrap the environment and install packages.
 rem ====================
 setlocal
 
-start /wait "bootstrapping..." cmd /c call "%~dp0bootstrap.bat"
-start /wait "choco-install packages-%~n0.config" cmd /c choco install "%~dp0packages-%~n0.config" /y 
+call "%~dp0bootstrap.bat"
+choco install "%~dp0packages-%~n0.config" /y 
 
 endlocal
