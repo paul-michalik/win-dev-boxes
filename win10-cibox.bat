41f2e428309a7474acbf2aeda9ec50d76ef1d74d
PMichalik <paul.michalik@harman.com>
2017-09-01 14:21:27 +0200

    Further simplification: all invocation scripts are identical and contain only one line.

--- win10-cibox.bat
+++ win10-cibox.bat
@@ -4,7 +4,6 @@ rem Bootstrap the environment and install packages.
 rem ====================
 setlocal
 
-call "%~dp0bootstrap.bat"
-choco install "%~dp0packages-%~n0.config" /y 
+call "%~dp0choco-install.bat" "%~dp0packages-%~n0.config" 
 
 endlocal
