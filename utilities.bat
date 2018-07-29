d21def260afbab09ec94498202fa7c3eb977bff1
Paul Michalik <paul.michalik@outlook.com>
2018-07-29 14:41:45 +0200

    Added concept for *.txt based line-by-line installs usable for upgrade and install

--- utilities.bat
+++ utilities.bat
@@ -4,6 +4,6 @@ rem Bootstrap the environment and install packages.
 rem ====================
 setlocal
 
-call "%~dp0choco-install.bat" "%~dp0packages-%~n0.config" 
+call "%~dp0choco-install-file.bat" "%~dp0packages-%~n0.txt" 
 
 endlocal
