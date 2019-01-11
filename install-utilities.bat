f85aaa8dae165923f3d285743bde03fdef25cfe3
Paul Michalik <paul.michalik@outlook.com>
2019-01-11 15:08:11 +0100

    Update and rename utilities.bat to install-utilities.bat

--- install-utilities.bat
+++ install-utilities.bat
@@ -4,6 +4,6 @@ rem Bootstrap the environment and install packages.
 rem ====================
 setlocal
 
-call "%~dp0choco-install-file.bat" "%~dp0packages-%~n0.txt" 
+call "%~dp0choco-run-command.bat" "%~dp0packages-%~n0.txt" install
 
 endlocal
